5f3271f894da8e304f5eadebcbaf66339ccd626b
Hiren Vadalia <hiren.vadalia@altair.com>
2020-04-30 15:45:50 +0530

    Part 8 - Libwire

--- src/include/batch_request.h
+++ src/include/batch_request.h
@@ -43,10 +43,103 @@ extern "C" {
 
 #include "pbs_share.h"
 #include "attribute.h"
-#include "libpbs.h"
 #include "net_connect.h"
 
 #define PBS_SIGNAMESZ 16
+
+#define BATCH_REPLY_CHOICE_NULL		1	/* no reply choice, just code */
+#define BATCH_REPLY_CHOICE_Queue	2	/* Job ID, see brp_jid */
+#define BATCH_REPLY_CHOICE_RdytoCom	3	/* select, see brp_jid */
+#define BATCH_REPLY_CHOICE_Commit	4	/* commit, see brp_jid */
+#define BATCH_REPLY_CHOICE_Select	5	/* select, see brp_select */
+#define BATCH_REPLY_CHOICE_Status	6	/* status, see brp_status */
+#define BATCH_REPLY_CHOICE_Text		7	/* text, see brp_txt */
+#define BATCH_REPLY_CHOICE_Locate	8	/* locate, see brp_locate */
+#define BATCH_REPLY_CHOICE_RescQuery	9	/* Resource Query */
+#define BATCH_REPLY_CHOICE_PreemptJobs	10	/* Preempt Job */
+
+/*
+ * The Batch Request ID numbers
+ */
+#define PBS_BATCH_Connect		0
+#define PBS_BATCH_QueueJob		1
+/* Unused -- #define PBS_BATCH_JobCred 2 */
+#define PBS_BATCH_jobscript		3
+#define PBS_BATCH_RdytoCommit		4
+#define PBS_BATCH_Commit		5
+#define PBS_BATCH_DeleteJob		6
+#define PBS_BATCH_HoldJob		7
+#define PBS_BATCH_LocateJob		8
+#define PBS_BATCH_Manager		9
+#define PBS_BATCH_MessJob		10
+#define PBS_BATCH_ModifyJob		11
+#define PBS_BATCH_MoveJob		12
+#define PBS_BATCH_ReleaseJob		13
+#define PBS_BATCH_Rerun			14
+#define PBS_BATCH_RunJob		15
+#define PBS_BATCH_SelectJobs		16
+#define PBS_BATCH_Shutdown		17
+#define PBS_BATCH_SignalJob		18
+#define PBS_BATCH_StatusJob		19
+#define PBS_BATCH_StatusQue		20
+#define PBS_BATCH_StatusSvr		21
+#define PBS_BATCH_TrackJob		22
+#define PBS_BATCH_AsyrunJob		23
+#define PBS_BATCH_Rescq			24
+#define PBS_BATCH_ReserveResc		25
+#define PBS_BATCH_ReleaseResc		26
+#define PBS_BATCH_FailOver		27
+#define PBS_BATCH_StageIn		48
+/* Unused -- #define PBS_BATCH_AuthenResvPort 49 */
+#define PBS_BATCH_OrderJob		50
+#define PBS_BATCH_SelStat		51
+#define PBS_BATCH_RegistDep		52
+#define PBS_BATCH_CopyFiles		54
+#define PBS_BATCH_DelFiles		55
+#define PBS_BATCH_JobObit		56
+#define PBS_BATCH_MvJobFile		57
+#define PBS_BATCH_StatusNode		58
+#define PBS_BATCH_Disconnect		59
+/* Unused -- #define PBS_BATCH_CopyFiles_Cred 60 */
+/* Unused -- #define PBS_BATCH_DelFiles_Cred 61 */
+#define PBS_BATCH_JobCred		62
+#define PBS_BATCH_CopyFiles_Cred	63
+#define PBS_BATCH_DelFiles_Cred		64
+/* Unused -- #define PBS_BATCH_GSS_Context 65 */
+#define PBS_BATCH_SubmitResv		70
+#define PBS_BATCH_StatusResv		71
+#define PBS_BATCH_DeleteResv		72
+#define PBS_BATCH_UserCred		73
+/* Unused -- #define PBS_BATCH_UserMigrate		74 */
+#define PBS_BATCH_ConfirmResv		75
+#define PBS_BATCH_DefSchReply		80
+#define PBS_BATCH_StatusSched		81
+#define PBS_BATCH_StatusRsc		82
+#define PBS_BATCH_StatusHook		83
+#define PBS_BATCH_PySpawn		84
+#define PBS_BATCH_CopyHookFile		85
+#define PBS_BATCH_DelHookFile		86
+/* Unused -- #define PBS_BATCH_MomRestart 87 */
+/* Unused -- #define PBS_BATCH_AuthExternal 88 */
+#define PBS_BATCH_HookPeriodic		89
+#define PBS_BATCH_RelnodesJob		90
+#define PBS_BATCH_ModifyResv		91
+#define PBS_BATCH_ResvOccurEnd		92
+#define PBS_BATCH_PreemptJobs		93
+#define PBS_BATCH_Cred			94
+#define PBS_BATCH_Authenticate		95
+#define PBS_BATCH_ModifyJob_Async	96
+
+#define PBS_BATCH_FileOpt_Default	0
+#define PBS_BATCH_FileOpt_OFlg		1
+#define PBS_BATCH_FileOpt_EFlg		2
+
+#define FAILOVER_Register	0 /* secondary server register with primary */
+#define FAILOVER_HandShake	1 /* handshake from secondary to primary */
+#define FAILOVER_PrimIsBack	2 /* Primary is taking control again */
+#define FAILOVER_SecdShutdown	3 /* Primary going down, secondary go down */
+#define FAILOVER_SecdGoInactive	4 /* Primary down, secondary go inactive */
+#define FAILOVER_SecdTakeOver	5 /* Primary down, secondary take over */
 
 /* QueueJob */
 struct rq_queuejob {
@@ -250,6 +343,12 @@ struct rqfpair {
 	char *fp_rmt;   /* used in Copy only     */
 };
 
+struct rq_preempt {
+	int count;
+	preempt_job_info *ppj_list;
+};
+typedef struct rq_preempt brp_preempt_jobs;
+
 /*
  * ok we now have all the individual request structures defined,
  * so here is the union ...
@@ -311,8 +410,62 @@ struct batch_request {
 		struct rq_cred rq_cred;
 	} rq_ind;
 };
-
 typedef struct batch_request breq;
+
+/* PBS Batch Reply Structure */
+
+/* reply to Select Job Request */
+struct brp_select {
+	struct brp_select *brp_next;
+	char brp_jobid[PBS_MAXSVRJOBID + 1];
+};
+
+/* reply to Status Job/Queue/Server Request */
+struct brp_status {
+	pbs_list_link brp_stlink;
+	int brp_objtype;
+	char brp_objname[(PBS_MAXSVRJOBID > PBS_MAXDEST ? PBS_MAXSVRJOBID : PBS_MAXDEST) + 1];
+	pbs_list_head brp_attr; /* head of svrattrlist */
+};
+
+struct brp_cmdstat {
+	struct brp_cmdstat *brp_stlink;
+	int brp_objtype;
+	char brp_objname[(PBS_MAXSVRJOBID > PBS_MAXDEST ? PBS_MAXSVRJOBID : PBS_MAXDEST) + 1];
+	struct attrl *brp_attrl;
+};
+
+/* reply to Resource Query Request */
+struct brp_rescq {
+	int brq_number; /* number of items in following arrays */
+	int *brq_avail;
+	int *brq_alloc;
+	int *brq_resvd;
+	int *brq_down;
+};
+
+/*
+ * the following is the basic Batch Reply structure
+ */
+struct batch_reply {
+	int brp_code;
+	int brp_auxcode;
+	int brp_choice; /* the union discriminator */
+	union {
+		char brp_jid[PBS_MAXSVRJOBID + 1];
+		struct brp_select *brp_select; /* select replies */
+		pbs_list_head brp_status; /* status (svr) replies */
+		struct brp_cmdstat *brp_statc; /* status (cmd) replies) */
+		struct {
+			int brp_txtlen;
+			char *brp_str;
+		} brp_txt; /* text and credential reply */
+		char brp_locate[PBS_MAXDEST + 1];
+		struct brp_rescq brp_rescq; /* query resource reply */
+		brp_preempt_jobs brp_preempt_jobs; /* preempt jobs reply */
+	} brp_un;
+};
+typedef struct batch_reply breply;
 
 extern breq *alloc_br(int);
 extern void reply_ack(struct batch_request *);
--- src/include/libpbs.h
+++ src/include/libpbs.h
@@ -51,6 +51,7 @@ extern "C" {
 #include "pbs_internal.h"
 #include "pbs_client_thread.h"
 #include "net_connect.h"
+#include "batch_request.h"
 
 /* Protocol types when connecting to another server (eg mom) */
 #define PROT_INVALID -1
@@ -131,155 +132,6 @@ pthread_mutex_t * get_conn_mutex(int);
 /* max number of preempt orderings */
 #define PREEMPT_ORDER_MAX 20
 
-/* PBS Batch Reply Structure */
-
-/* reply to Select Job Request */
-struct brp_select {
-	struct brp_select *brp_next;
-	char brp_jobid[PBS_MAXSVRJOBID + 1];
-};
-
-/* reply to Status Job/Queue/Server Request */
-struct brp_status {
-	pbs_list_link brp_stlink;
-	int brp_objtype;
-	char brp_objname[(PBS_MAXSVRJOBID > PBS_MAXDEST ? PBS_MAXSVRJOBID : PBS_MAXDEST) + 1];
-	pbs_list_head brp_attr; /* head of svrattrlist */
-};
-
-struct brp_cmdstat {
-	struct brp_cmdstat *brp_stlink;
-	int brp_objtype;
-	char brp_objname[(PBS_MAXSVRJOBID > PBS_MAXDEST ? PBS_MAXSVRJOBID : PBS_MAXDEST) + 1];
-	struct attrl *brp_attrl;
-};
-
-/* reply to Resource Query Request */
-struct brp_rescq {
-	int brq_number; /* number of items in following arrays */
-	int *brq_avail;
-	int *brq_alloc;
-	int *brq_resvd;
-	int *brq_down;
-};
-
-struct rq_preempt {
-	int count;
-	preempt_job_info *ppj_list;
-};
-
-typedef struct rq_preempt brp_preempt_jobs;
-
-#define BATCH_REPLY_CHOICE_NULL		1	/* no reply choice, just code */
-#define BATCH_REPLY_CHOICE_Queue	2	/* Job ID, see brp_jid */
-#define BATCH_REPLY_CHOICE_RdytoCom	3	/* select, see brp_jid */
-#define BATCH_REPLY_CHOICE_Commit	4	/* commit, see brp_jid */
-#define BATCH_REPLY_CHOICE_Select	5	/* select, see brp_select */
-#define BATCH_REPLY_CHOICE_Status	6	/* status, see brp_status */
-#define BATCH_REPLY_CHOICE_Text		7	/* text, see brp_txt */
-#define BATCH_REPLY_CHOICE_Locate	8	/* locate, see brp_locate */
-#define BATCH_REPLY_CHOICE_RescQuery	9	/* Resource Query */
-#define BATCH_REPLY_CHOICE_PreemptJobs	10	/* Preempt Job */
-
-/*
- * the following is the basic Batch Reply structure
- */
-struct batch_reply
-{
-	int brp_code;
-	int brp_auxcode;
-	int brp_choice; /* the union discriminator */
-	union {
-		char brp_jid[PBS_MAXSVRJOBID + 1];
-		struct brp_select *brp_select; /* select replies */
-		pbs_list_head brp_status; /* status (svr) replies */
-		struct brp_cmdstat *brp_statc; /* status (cmd) replies) */
-		struct {
-			int brp_txtlen;
-			char *brp_str;
-		} brp_txt; /* text and credential reply */
-		char brp_locate[PBS_MAXDEST + 1];
-		struct brp_rescq brp_rescq; /* query resource reply */
-		brp_preempt_jobs brp_preempt_jobs; /* preempt jobs reply */
-	} brp_un;
-};
-
-/*
- * The Batch Request ID numbers
- */
-#define PBS_BATCH_Connect		0
-#define PBS_BATCH_QueueJob		1
-/* Unused -- #define PBS_BATCH_JobCred 2 */
-#define PBS_BATCH_jobscript		3
-#define PBS_BATCH_RdytoCommit		4
-#define PBS_BATCH_Commit		5
-#define PBS_BATCH_DeleteJob		6
-#define PBS_BATCH_HoldJob		7
-#define PBS_BATCH_LocateJob		8
-#define PBS_BATCH_Manager		9
-#define PBS_BATCH_MessJob		10
-#define PBS_BATCH_ModifyJob		11
-#define PBS_BATCH_MoveJob		12
-#define PBS_BATCH_ReleaseJob		13
-#define PBS_BATCH_Rerun			14
-#define PBS_BATCH_RunJob		15
-#define PBS_BATCH_SelectJobs		16
-#define PBS_BATCH_Shutdown		17
-#define PBS_BATCH_SignalJob		18
-#define PBS_BATCH_StatusJob		19
-#define PBS_BATCH_StatusQue		20
-#define PBS_BATCH_StatusSvr		21
-#define PBS_BATCH_TrackJob		22
-#define PBS_BATCH_AsyrunJob		23
-#define PBS_BATCH_Rescq			24
-#define PBS_BATCH_ReserveResc		25
-#define PBS_BATCH_ReleaseResc		26
-#define PBS_BATCH_FailOver		27
-#define PBS_BATCH_StageIn		48
-/* Unused -- #define PBS_BATCH_AuthenResvPort 49 */
-#define PBS_BATCH_OrderJob		50
-#define PBS_BATCH_SelStat		51
-#define PBS_BATCH_RegistDep		52
-#define PBS_BATCH_CopyFiles		54
-#define PBS_BATCH_DelFiles		55
-#define PBS_BATCH_JobObit		56
-#define PBS_BATCH_MvJobFile		57
-#define PBS_BATCH_StatusNode		58
-#define PBS_BATCH_Disconnect		59
-/* Unused -- #define PBS_BATCH_CopyFiles_Cred 60 */
-/* Unused -- #define PBS_BATCH_DelFiles_Cred 61 */
-#define PBS_BATCH_JobCred		62
-#define PBS_BATCH_CopyFiles_Cred	63
-#define PBS_BATCH_DelFiles_Cred		64
-/* Unused -- #define PBS_BATCH_GSS_Context 65 */
-#define PBS_BATCH_SubmitResv		70
-#define PBS_BATCH_StatusResv		71
-#define PBS_BATCH_DeleteResv		72
-#define PBS_BATCH_UserCred		73
-/* Unused -- #define PBS_BATCH_UserMigrate		74 */
-#define PBS_BATCH_ConfirmResv		75
-#define PBS_BATCH_DefSchReply		80
-#define PBS_BATCH_StatusSched		81
-#define PBS_BATCH_StatusRsc		82
-#define PBS_BATCH_StatusHook		83
-#define PBS_BATCH_PySpawn		84
-#define PBS_BATCH_CopyHookFile		85
-#define PBS_BATCH_DelHookFile		86
-/* Unused -- #define PBS_BATCH_MomRestart 87 */
-/* Unused -- #define PBS_BATCH_AuthExternal 88 */
-#define PBS_BATCH_HookPeriodic		89
-#define PBS_BATCH_RelnodesJob		90
-#define PBS_BATCH_ModifyResv		91
-#define PBS_BATCH_ResvOccurEnd		92
-#define PBS_BATCH_PreemptJobs		93
-#define PBS_BATCH_Cred			94
-#define PBS_BATCH_Authenticate		95
-#define PBS_BATCH_ModifyJob_Async	96
-
-#define PBS_BATCH_FileOpt_Default	0
-#define PBS_BATCH_FileOpt_OFlg		1
-#define PBS_BATCH_FileOpt_EFlg		2
-
 #define PBS_IFF_CLIENT_ADDR "PBS_IFF_CLIENT_ADDR"
 
 /* time out values for tcp_dis read/write */
@@ -289,13 +141,6 @@ struct batch_reply
 #define PBS_DIS_TCP_TIMEOUT_RERUN	45 /* timeout used in pbs_rerunjob() */
 #define PBS_DIS_TCP_TIMEOUT_LONG	600
 #define PBS_DIS_TCP_TIMEOUT_VLONG	10800
-
-#define FAILOVER_Register	0 /* secondary server register with primary */
-#define FAILOVER_HandShake	1 /* handshake from secondary to primary */
-#define FAILOVER_PrimIsBack	2 /* Primary is taking control again */
-#define FAILOVER_SecdShutdown	3 /* Primary going down, secondary go down */
-#define FAILOVER_SecdGoInactive	4 /* Primary down, secondary go inactive */
-#define FAILOVER_SecdTakeOver	5 /* Primary down, secondary take over */
 
 extern int is_compose(int, int);
 extern int is_compose_cmd(int, int, char **);
